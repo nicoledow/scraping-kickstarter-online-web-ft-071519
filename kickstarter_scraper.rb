ebd80660f03b4cf9da98f79f30e4f641b0de2d95
Nicole Dow <nikki.dow71@gmail.com>
2019-08-01 21:11:38 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,24 +1,24 @@
 require 'nokogiri'
 require 'pry'
 
-def create_project_hash
-  html = File.read('fixtures/kickstarter.html')
-  kickstarter = Nokogiri::HTML(html)
-  projects = {}
+# def create_project_hash
+#   html = File.read('fixtures/kickstarter.html')
+#   kickstarter = Nokogiri::HTML(html)
+#   projects = {}
   
-  #collects projects, makes the title of each a key within the projects hash pointing to an empty hash
-  kickstarter.css("li.project.grid_4").each do |project|
-    title = kickstarter.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {
-      :image_link => kickstarter.css("div.project-thumbnail a img").attribute("src").value,
-      :description => kickstarter.css("p.bbcard_blurb").text,
-      :location => kickstarter.css("ul .project-meta span.location-name").text,
-      :percent_funded => kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
-    }
-  end
+#   #collects projects, makes the title of each a key within the projects hash pointing to an empty hash
+#   kickstarter.css("li.project.grid_4").each do |project|
+#     title = kickstarter.css("h2.bbcard_name strong a").text
+#     projects[title.to_sym] = {
+#       :image_link => kickstarter.css("div.project-thumbnail a img").attribute("src").value,
+#       :description => kickstarter.css("p.bbcard_blurb").text,
+#       :location => kickstarter.css("ul .project-meta span.location-name").text,
+#       :percent_funded => kickstarter.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+#     }
+#   end
   
-  projects
-end
+#   projects
+# end
 
 #create_project_hash
 
