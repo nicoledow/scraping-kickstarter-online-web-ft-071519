447f51590df23f66b9fb37a6024c147a9e701299
Nicole Dow <nikki.dow71@gmail.com>
2019-08-01 20:46:21 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -12,3 +12,4 @@ create_project_hash
 # projects: kickstarter.css("li.project.grid_4")
 #titles: kickstarter.css("h2.bbcard_name strong a").text
 # image link: kickstarter.css("div.project-thumbnail a img").attribute("src").value
+#descriptions: kickstarter.css("p.bbcard_blurb")
